b9e485d5e99272e10433c5ac8278aa463cefcc09
Dr Nic Williams <drnicwilliams@gmail.com>
2010-12-30 08:51:05 -0800

    set the final server URL

--- config/initializers/demo_day_server.rb
+++ config/initializers/demo_day_server.rb
@@ -1,1 +1,2 @@
-Rails.configuration.demo_day_server = YAML.load_file(File.join(Rails.root, "config", "demo_day_server.yml"))[:server_url]
+Rails.configuration.demo_day_server = ENV['DEMO_DAY_SERVER']
+Rails.configuration.demo_day_server ||= YAML.load_file(File.join(Rails.root, "config", "demo_day_server.yml"))[:server_url]
