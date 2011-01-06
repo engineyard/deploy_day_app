c032330295fda77e2a69b33abd8823bccc23f5ca
Dr Nic Williams <drnicwilliams@gmail.com>
2011-01-05 17:47:59 -0800

    updated the server URL; error message for basic auth failure

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## Deploying
 
 This is a Rails 3 application (rails 3.0.3 if you need it for Engine Yard).
+
--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -3,6 +3,9 @@ class ParticipationsController < ApplicationController
     begin
       @participation = Participation.new
       self.user_id = @participation.deployed!(request.host)
+    rescue ActiveResource::UnauthorizedAccess
+      flash.now[:notice] = "Argh. The server password has changed. Please tell Dr Nic. Do not submit your form."
+      render :action => "index"
     rescue Errno::ECONNREFUSED, Errno::EAFNOSUPPORT
       flash.now[:notice] = "Cannot connect to central server #{ParticipationResource.site}. Please tell Dr Nic. Do not submit your form."
     rescue Exception => e
@@ -23,6 +26,9 @@ class ParticipationsController < ApplicationController
           flash[:notice] = "Please fill in all fields"
           render :action => "index"
         end
+      rescue ActiveResource::UnauthorizedAccess
+        flash.now[:notice] = "Argh. The server password has changed. Please tell Dr Nic. Do not submit your form."
+        render :action => "index"
       rescue Errno::ECONNREFUSED, Errno::EAFNOSUPPORT
         flash.now[:notice] = "Cannot connect to central server #{ParticipationResource.site}. Please tell Dr Nic. Do not submit your form."
         render :action => "index"
